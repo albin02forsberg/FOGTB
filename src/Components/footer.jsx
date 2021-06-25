54d79649aab516418e9447a34805eaaa3e457ec1
Albin Forsberg <albin02forsberg@gmail.com>
2021-06-25 12:37:05 +0200

    Update footer.jsx

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className="content has-text-centered mb-0">
         <p>
           <strong>FOGTB</strong> by 
-          <a href="https://github.com/albin02forsberg"> Albin Forsberg</a>.
+          <a href="https://www.linkedin.com/in/albin-forsberg/"> Albin Forsberg</a>.
           This project is open source and can be found on <a href="https://github.com/albin02forsberg/fogtb">Github</a>.
         </p>
       </div>
